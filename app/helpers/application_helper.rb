094bea555e66297241fbd7337d62b9500aa0d9ba
Ingrid Molloy <ingrid.molloy@shopify.com>
2016-12-06 15:28:00 -0800

    minor changes mostly doing and undoing while following the tutorial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  
+  # Returns full title for the page
+  def full_title(page_title = '')
+    base_title = "Bettwitter the Amazing Twitter Clone"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,22 +1,29 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @base_title = "Bettwitter &mdash; the Amazing Twitter Clone"
+  end
+
   test "should get home" do
-    get static_pages_home_url
+    get :home
+    # get static_pages_home_url
     assert_response :success # Checks for 200 Ok status
-    assert_select "title", "Home | Bettwitter"
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get :help
+    # get static_pages_help_url
     assert_response :success
-    assert_select "title", "Help | Bettwitter"
+    assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
-    get static_pages_about_url
+    get :about
+    # get static_pages_about_url
     assert_response :success
-    assert_select "title", "About | Bettwitter"
+    assert_select "title", "About | #{@base_title}"
   end
 
 end
