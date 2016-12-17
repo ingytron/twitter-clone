3f0431a8346a7633f24bfb22f7d549c2220ffba9
Ingrid Molloy <ingrid.molloy@shopify.com>
2016-12-16 17:09:45 -0800

    Finished making user signup which appears to not work but yolo

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+
+  # Get the user's gravatar
+  def gravatar_for
+    gravatar_id = Digest::MD5::hexidigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
 end
