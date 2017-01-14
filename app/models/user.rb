cfceda23b8dc7e017ae3a3e3554f13222eabf43d
Ingrid Molloy <ingrid.molloy@shopify.com>
2017-01-13 16:24:10 -0800

    bit more work on login / remember

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,17 +9,20 @@ class User < ApplicationRecord
             uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+  class << self
+    #Returns the hash digest of the given string
+    #def User.digest(string)
+    def digest(string)
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                    BCrypt::Engine.cost
+      BCrypt::Password.create(string, cost: cost)
+    end
 
-  #Returns the hash digest of the given string
-  def User.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                  BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost: cost)
-  end
-
-  #returns a random token
-  def User.new_token
-    SecureRandom.urlsafe_base64
+    #returns a random token
+    def new_token
+    #def User.new_token
+      SecureRandom.urlsafe_base64
+    end
   end
 
   #Remembers a user in the database for use in persistent sessions
